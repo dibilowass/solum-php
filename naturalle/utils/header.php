b926da9386c83cfb137706a100929595d0908cf0
dibilowas <168019352+dibilowass@users.noreply.github.com>
2025-03-08 12:19:07 -0300

    Add files via upload

---
+++ naturalle/utils/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Naturalle</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+    crossorigin="anonymous">
+<!-- css -->
+    <link rel="stylesheet" href="css/felly.css">
+    <link rel="stylesheet" href="css/nally.css">
+
+<!-- montserrat -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+<!-- league spartan -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+
---
+++ naturalle/utils/navbar.php
@@ -0,0 +1,51 @@
+<div class="nav"> 
+    
+<div class="navlogo">
+  <a href="index.php"> <img src="assets\imagens\naturalle.png"> </a>
+</div>
+
+
+<div class="navlink">
+   <input type="search" placeholder="Buscar" class="navpesquisa" name="buscar"  id="buscar">
+</div>
+
+<div class="navlink">
+    <img src="assets\icones\carrinho.png">
+</div>
+
+
+<div class="navlink">
+   <a href="cadastro.php"> <img src="assets\icones\entrar.png"> </a>
+</div>
+
+
+</div>
+
+<div class="navcategoria"> 
+
+
+
+<div class="navlinkcategoria">
+Nos contate
+</div>
+
+
+<div class="dropdown">
+  <button class="dropbtn">Categorias</button>
+  <div class="dropdown-content">
+    <a href="#">em construção</a>
+    <a href="#">em construção</a>
+    <a href="#">em construção</a>
+  </div>
+</div>
+
+
+
+<div class="navlinkcategoria">
+    Vender
+
+
+</div>
+
+
+</div>
